04145bd24f03c94f43c609ed6bee7a989472b346
Alok <alokjnp729@gmail.com>
2024-02-23 05:50:32 +0530

    completed

--- server/routes/VisionRoutes.js
+++ server/routes/VisionRoutes.js
@@ -1,18 +1,21 @@
 import express from "express";
 import *as dotenv from 'dotenv';
-import { OpenAI } from 'openai';
-
+// import { OpenAI } from 'openai';
+import { Configuration, OpenAIApi } from "openai";
 
 
 
 dotenv.config();
 const router=express.Router();
 
-const openai = new OpenAI({
-    apiKey:process.env.OPENAI_API_KEY,
-})
+// const openai = new OpenAI({
+//     apiKey:process.env.OPENAI_API_KEY,
+// })
 //const openai=new OpenAIApi(configuration);
-
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  const openai = new OpenAIApi(configuration);
 router.route('/').get((req,res)=>{
     res.send('Hello from VisionCraft');
 })
